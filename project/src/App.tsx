3b0171593ea5a96e2960a29b4aafe3ab51af5db8
RealZN <71386797+TbhX@users.noreply.github.com>
2024-11-22 11:46:31 +0100

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -5,10 +5,16 @@ import Stats from './components/Stats';
 import Location from './components/Location';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import LanguageSwitcher from './components/LanguageSwitcher'; // Ajoutez l'importation
 
 function App() {
   return (
     <div className="min-h-screen bg-black text-white">
+      {/* Placez le composant LanguageSwitcher où vous souhaitez */}
+      <div className="absolute top-4 right-4 z-10">
+        <LanguageSwitcher text="Welcome to Le91 Arena, the ultimate gaming experience!" />
+      </div>
+      
       <Hero />
       <About />
       <Stats />
