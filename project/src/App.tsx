86aa22b61ea150aa374162f3108d2a6d15931a8a
RealZN <71386797+TbhX@users.noreply.github.com>
2024-11-26 06:47:01 +0100

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,8 +1,11 @@
+// src/App.tsx
 import React from 'react';
 import Hero from './components/Hero';
 import About from './components/About';
 import Stats from './components/Stats';
 import Location from './components/Location';
+import Contact from './components/Contact';  // Import du composant Contact
+import Footer from './components/Footer';  // Import du composant Footer
 
 const App = () => {
   return (
@@ -11,6 +14,8 @@ const App = () => {
       <About />
       <Stats />
       <Location />
+      <Contact />  {/* Utilisation du composant Contact */}
+      <Footer />   {/* Utilisation du composant Footer */}
     </div>
   );
 };
