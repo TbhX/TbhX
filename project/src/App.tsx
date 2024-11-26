c5851bb646fa2a358bc4cea2d568319bc4cee284
RealZN <71386797+TbhX@users.noreply.github.com>
2024-11-26 06:42:44 +0100

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -3,21 +3,16 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Stats from './components/Stats';
 import Location from './components/Location';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
 
-
-function App() {
+const App = () => {
   return (
-      
+    <div>
       <Hero />
       <About />
       <Stats />
       <Location />
-      <Contact />
-      <Footer />
     </div>
   );
-}
+};
 
 export default App;
