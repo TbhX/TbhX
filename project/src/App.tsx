2e7389994088840fce87babc44a2973a95188a8c
RealZN <71386797+TbhX@users.noreply.github.com>
2024-11-27 01:05:15 +0100

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import React from 'react';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -6,10 +5,12 @@ import Stats from './components/Stats';
 import Location from './components/Location';
 import Contact from './components/Contact';  // Import du composant Contact
 import Footer from './components/Footer';  // Import du composant Footer
+import LanguageSwitcher from './components/LanguageSwitcher'; // Import du composant LanguageSwitcher
 
 const App = () => {
   return (
     <div>
+      <LanguageSwitcher /> {/* Add the LanguageSwitcher at the top */}
       <Hero />
       <About />
       <Stats />
